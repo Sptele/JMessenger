2d1dfd071b8dbf777c318e820835c0df25135a63
Gautam <gautamkhaj07@gmail.com>
2022-06-17 22:57:09 -0700

    gitignore test

--- src/main/java/org/sptele/JMessenger/Sender.java
+++ src/main/java/org/sptele/JMessenger/Sender.java
@@ -3,7 +3,9 @@ package org.sptele.JMessenger;
 import org.sptele.JMessenger.Entities.Message;
 
 public class Sender {
+
     public static boolean sendMessage(Message msg) {
+
         System.out.println("Will send " + msg.toString());
 
         return true;
